4f0bd8d26971a51cf13bb66f858d871c7bb9d737
Pavel Tarasov <gomucyl@gmail.com>
2023-02-19 22:48:17 +0100

    Initial commit

---
+++ src/services/auth.service.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import {response} from "msw";
+
+const API_URL = "http://localhost:35625/Auth";
+
+ class AuthService {
+     login(email: string, password: string) {
+        return axios.post(API_URL + "Login",
+            {
+                email,
+                password
+            })
+            .then(response => {
+                if (response.data.accessToken) {
+                    localStorage.setItem("user", JSON.stringify(response.data));
+                }
+
+                return response.data;
+            });
+    }
+
+    logout() {
+        localStorage.removeItem("user");
+    }
+
+    register(username: string, email: string, password: string) {
+        return axios.post(API_URL + "Register", {
+            username,
+            email,
+            password
+        });
+    }
+
+    getCurrentUser() {
+        const userStr = localStorage.getItem("user");
+        if (userStr){
+            return JSON.parse(userStr);
+        }
+
+        return null;
+    }
+}
+
+export default new AuthService();
